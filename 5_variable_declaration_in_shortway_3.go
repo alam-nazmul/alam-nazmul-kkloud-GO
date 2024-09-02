f7f5d95e5ae765d0b2d92bbdac5350cee557805d
alam-nazmul <alam.mdnazmul@outlook.com>
2024-09-03 00:59:03 +0600

    variable call by short way

---
+++ 5_variable_declaration_in_shortway_3.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Nazmul" // This is a string variable
+	name = "Alam"    // You can modify this variable by using on string variable
+	fmt.Println(name)
+}
