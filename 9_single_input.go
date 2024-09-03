ed000f069c0c139a8e7c68313341c443b4756722
nazmul.alam@cellosco.pe <nazmul.alam@cellosco.pe>
2024-09-03 11:04:13 +0600

    Comment added

--- 9_single_input.go
+++ 9_single_input.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	var name string
 	fmt.Printf("Enter your name: ")
-	fmt.Scanf("%s", &name) // Here "&" is a pointer
+	fmt.Scanf("%s", &name) // Here "&" is a pointer as a scanner
 	fmt.Println("Hey there, ", name)
 }
