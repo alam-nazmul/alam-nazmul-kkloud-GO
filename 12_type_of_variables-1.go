5966f05aaaef1809bff85784886bc4907177f204
nazmul.alam@cellosco.pe <nazmul.alam@cellosco.pe>
2024-09-04 11:53:45 +0600

    Variable type

---
+++ 12_type_of_variables-1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	grade := 32
+	message := "Hello World"
+	isCheck := true
+	amount := 67.33
+	fmt.Printf("Variable grade = %v is of type %T \n", grade, grade)
+	fmt.Printf("Variable message = %v is of type %T \n", message, message)
+	fmt.Printf("variable isCheck = %v is of type %T \n", isCheck, isCheck)
+	fmt.Printf("variable amount = %v is of type %T", amount, amount)
+}
