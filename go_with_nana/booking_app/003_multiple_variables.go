b561b1568281a8b931a906bb107cf987f26d6794
alam-nazmul <alam.mdnazmul@outlook.com>
2025-03-13 04:22:17 +0600

    How to call the multiple variables

---
+++ go_with_nana/booking_app/003_multiple_variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Programming"
+	totalTickets := 150
+	remainingTickets := 5
+
+	fmt.Println("Welcome to", conferenceName, "language!")
+	fmt.Println("We have total of", totalTickets, "conference tickets and")
+	fmt.Println("we have remaining of", remainingTickets, "conference tickets")
+	fmt.Println("Get your tickets here to attend")
+}
